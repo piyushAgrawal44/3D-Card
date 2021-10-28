05967a4702028c9e9ab40bbab19ed738c60906e5
piyushAgrawal44 <90279467+piyushAgrawal44@users.noreply.github.com>
2021-10-28 09:27:26 +0530

    all new

--- index.js
+++ index.js
@@ -1,17 +1,30 @@
 var oldhtml = document.getElementById('body').innerHTML;
-function piyush()
+function secondpage()
 {
     document.getElementById("firstpage").style.transform = "rotateY(-180deg)";
     document.getElementById("firstpagebackface").style.transform = "rotateY(180deg)";
     document.getElementById("container").style.transform = "translateX(120px)";
 }
 
-function aayush()
+function undo1(){
+    document.getElementById("firstpage").style.transform = "rotateY(0deg)";
+    document.getElementById("firstpagebackface").style.transform = "rotateY(0deg)";
+    document.getElementById("container").style.transform = "translateX(0px)";
+}
+
+function lastpage()
 {
     document.getElementById("secondpage").style.transform = "rotateY(-180deg)";
     document.getElementById("secondpagebackface").style.transform = "rotateY(180deg)";
     document.getElementById("secondpagebackface").style.zIndex ="2";
 }
+
+function undo2(){
+    document.getElementById("secondpage").style.transform = "rotateY(0deg)";
+    document.getElementById("secondpagebackface").style.transform = "rotateY(0deg)";
+    document.getElementById("secondpagebackface").style.zIndex ="1";
+}
+
 function gotofront()
 {
     document.getElementById('body').innerHTML=oldhtml;
